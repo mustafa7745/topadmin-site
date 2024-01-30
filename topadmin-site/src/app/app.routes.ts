3d0ed549322a72545b1bdd614caeb317d8561e75
Mustafa Ismail <mustafa774519161@gmail.com>
2024-01-30 03:22:25 -0800

    add route component

--- topadmin-site/src/app/app.routes.ts
+++ topadmin-site/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { InitComponent } from './init/init.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'init', component: InitComponent },
+    { path: '', redirectTo: 'init', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+
+    { path: '**', component: NotFoundComponent },
+];
